639c3823be41d84dbe7bd32fe296a4580b2f0693
Brett <brett_matsumoto@yahoo.com>
2019-04-22 19:07:57 -1000

    adjusted per pull requests

--- MatrixRotator.js
+++ MatrixRotator.js
@@ -19,9 +19,9 @@ module.exports = class MatrixRotator {
   //      |-- Must be Direction.CW
   //      v        or Direction.CCW
   rotate(direction) {
-    // do work here
 
     let newArray = [];
+
     for (let i = 0; i < this.matrix.length; i++) {
       newArray[i] = this.matrix[i].slice();
     }
@@ -30,16 +30,16 @@ module.exports = class MatrixRotator {
       for (let i = 0; i < this.matrix.length; i++) {
         for (let j = 0; j < this.matrix[0].length; j++) {
           let changeRow = j - i;
-          let changeCollumn = this.matrix.length - 1 - (j + i);
-          newArray[i + changeRow][j + changeCollumn] = this.matrix[i][j];
+          let changeColumn = this.matrix.length - 1 - (j + i);
+          newArray[i + changeRow][j + changeColumn] = this.matrix[i][j];
         }
       }
     } else if (direction === 'CounterClockWise') {
       for (let i = 0; i < this.matrix.length; i++) {
         for (let j = 0; j < this.matrix[0].length; j++) {
-          let changeCollumn = i - j;
+          let changeColumn = i - j;
           let changeRow = this.matrix.length - 1 - (j + i);
-          newArray[i + changeRow][j + changeCollumn] = this.matrix[i][j];
+          newArray[i + changeRow][j + changeColumn] = this.matrix[i][j];
         }
       }
     }
